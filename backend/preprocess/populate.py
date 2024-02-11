f74759476d12e17d773b264720bd679d62236709
Sriman Goel <goelsriman@gmail.com>
2024-02-10 19:36:21 -0500

    updated population script

--- backend/preprocess/populate.py
+++ backend/preprocess/populate.py
@@ -3,18 +3,25 @@ import backend.image_process as ip
 import sys
 from multiprocessing import Pool
 
+def process_cloth(tup):
+    i, product_link, outfits_image_links, image_link = tup
+    print(i, "started")
+    print(product_link, outfits_image_links, image_link)
+    ip.process_scraped_image(image_link, outfits_image_links, product_link)
+    print(i, "ended")
+
 def process_clothes_pickle(path: str, num_processes: int = 1):
     data = None
     with open(path, "rb") as f:
         data = pickle.load(f)
 
-    def process_image(product_link):
-        outfits_image_links, image_link = data[product_link]
-        print(product_link, outfits_image_links, image_link)
-        ip.process_scraped_image(image_link, outfits_image_links, product_link)
+    inputs = []
+    for product_link in data.keys():
+        if data[product_link] is not None:
+            inputs.append((len(inputs), product_link, data[product_link][0], data[product_link][1]))
         
     with Pool(num_processes) as p:
-        p.map(process_image, data.keys())
+        p.map(process_cloth, inputs)
 
 if __name__ == "__main__":
     pickle_file_path = sys.argv[1]
